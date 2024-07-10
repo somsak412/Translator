82085fd4b59ebb0115e4039fa2c2a326acee8b37
somsak412 <50792207+somsak412@users.noreply.github.com>
2024-07-10 23:45:52 +0700

    Text Translator ASP.NET MVC

---
+++ TranslateASPMVC/Controllers/TranslateController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TranslateASPMVC.Models;
+
+namespace TranslateASPMVC.Controllers
+{
+    public class TranslateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(string inputText)
+        {
+            if (string.IsNullOrEmpty(inputText))
+            {
+                ViewBag.TranslatedText = "Please enter text to translate.";
+                return View();
+            }
+            
+            var translator = new Translator { OriginalText = inputText };
+            translator.Translate();
+
+            ViewBag.TranslatedText = translator.TranslatedText;
+            return View();
+        }
+        
+    }
+}
---
+++ TranslateASPMVC/Models/Translator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslateASPMVC.Models
+{
+    public class Translator
+    {
+        public string OriginalText { get; set; }
+        public string TranslatedText { get; private set; }
+        
+        public void Translate()
+        {
+            if (string.IsNullOrEmpty(OriginalText))
+            {
+                TranslatedText = "UBCO ";
+                return;
+            }
+
+            string vowels = "aeiouAEIOU";
+            string consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+
+            char NextConsonant(char c)
+            {
+                int index = consonants.IndexOf(c);
+                if (index == -1)
+                    return c; // not a consonant
+
+                return consonants[(index + 1) % consonants.Length];
+            }
+
+            var translatedBuilder = new System.Text.StringBuilder("UBCO ");
+
+            foreach (char c in OriginalText)
+            {
+                if (vowels.Contains(c))
+                {
+                    translatedBuilder.Append(c).Append(c);
+                }
+                else if (char.IsLetter(c))
+                {
+                    translatedBuilder.Append(NextConsonant(c));
+                }
+                else
+                {
+                    translatedBuilder.Append(c);
+                }
+            }
+
+            int wordCount = OriginalText.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            translatedBuilder.Append(wordCount);
+
+            TranslatedText = translatedBuilder.ToString();
+        }
+    }
+
+}
